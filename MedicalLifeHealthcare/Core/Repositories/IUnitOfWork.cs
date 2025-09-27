7258af4bb0ffdefddeda2c52f032a683b5c7076d
Vuyani Nombula <114491836+King-Shaka@users.noreply.github.com>
2025-09-27 23:04:34 +0200

    Delete MedicalLifeHealthcare directory

--- MedicalLifeHealthcare/Core/Repositories/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MedicalLifeHealthcare.Core.Repositories
-{
-    public interface IUnitOfWork
-    {
-        IUserRepository User { get; }
-
-        IRoleRepository Role { get; }
-    }
-}
--- MedicalLifeHealthcare/Models/Labs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MedicalLifeHealthcare.Models
-{
-    public class Labs
-    {
-        public int Id { get; set; }
-        public int TestId { get; set; }
-        public string ResultDetails { get; set; }
-    }
-}
